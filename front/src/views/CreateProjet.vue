3ed819910bcbf7c4cf407331f98c6f91a55c102a
Huerta Lucas <huertalucas13@gmail.com>
2022-09-22 17:45:22 +0200

    Form créer projet avec lien table user

--- front/src/views/CreateProjet.vue
+++ front/src/views/CreateProjet.vue
@@ -1,31 +1,44 @@
 <template>
     <div>
-        <h1>
+        <h1 class="text-center text-lg">
             Create a Projet 
         </h1>
 
-        <form method="post">
+        <form method="post" class="my-3 flex flex-col w-screen align-middle justify-evenly">
             <!-- INPUT AUTEUR -->
-            <div>
+            <div class="my-3 flex flex-row justify-evenly align-middle">
                 <label>
                     Nom Projet
                 </label>
-                <input type="text"
-                    v-model="nomProjet"
+                <input class="border-solid border-black bg-slate-100"
+                    v-model="nomProjet" 
                     />
             </div>
 
             <!-- INPUT projet -->
-            <div>
+            <div class="my-3 flex flex-row justify-evenly align-middle">
                 <label>
                     Domaine Projet
                 </label>
-                <input type="text" 
+                <input type="text" class="border-solid border-black bg-slate-100"
                     v-model="domaineProjet"
                     />
             </div>
 
-            <input type="submit" v-on:click="NewData()" value="Envoyer"/>
+            <div class="my-3 flex flex-row justify-evenly align-middle">
+                <label>
+                    Utilisateurs associés 
+                </label>
+                <select class="border-solid border-black bg-slate-100"
+                    v-model="usersProjets" v-for="(dataU, index) in dataUsers" :key="index"
+                    >
+                    <option>
+                        {{ dataU }}
+                    </option>
+                </select>
+            </div>
+
+            <input type="submit" class="btn w-1/5 m-auto rounded-md bg-blue-300" v-on:click="NewData()" value="Envoyer"/>
         </form>
     </div>
 </template>
@@ -36,7 +49,8 @@
         data() {
             return {
                 nomProjet: null, 
-                domaineProjet: null
+                domaineProjet: null, 
+                usersProjets: null
             };
         },
 
@@ -44,7 +58,8 @@
             NewData() {
                 axios.post('http://localhost:3000/projets/new',{
                     nomProjet: this.nomProjet, 
-                    domaineProjet: this.domaineProjet
+                    domaineProjet: this.domaineProjet, 
+                    usersProjets: this.usersProjets
                 },{
                     'content-type': 'application/json', 
                     'Access-Control-Allow-Origin': '*'
